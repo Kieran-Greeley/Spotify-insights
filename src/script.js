c762af2e0eccb834f64b49c545607096bd2795f3
Kieran-Greeley <kieran.greeley@gmail.com>
2024-06-19 14:21:56 -0700

    added bar graph

--- src/script.js
+++ src/script.js
@@ -103,8 +103,12 @@ function populateUI(profile, artists) {
     document.getElementById("url").innerText = profile.href;
     document.getElementById("url").setAttribute("href", profile.href);
 
+    let artist_name = [];
+    let artist_pop = [];
     let average_pop = 0;
     for(let i of artists.items){
+        artist_name.push(i.name);
+        artist_pop.push(i.popularity);
         let artist = document.createElement("li");
         artist.innerHTML = i.name + " " + " " + i.popularity;
         document.getElementById("top-artists").appendChild(artist);
@@ -114,4 +118,68 @@ function populateUI(profile, artists) {
     let avg = document.createElement("p");
     avg.innerHTML = "your average popularity score is: " + average_pop;
     document.getElementById("artists").appendChild(avg);
+
+    const ctx = document.getElementById('artists-graph');
+
+    new Chart(ctx, {
+        type: 'bar',
+        data: {
+          labels: artist_name,
+          datasets: [{
+            label: 'Popularity Score',
+            data: artist_pop,
+            backgroundColor: 'rgba(20, 205, 86, 1)',
+            //borderColor: 'rgb(20, 205, 86)',
+            borderWidth: 1
+          }]
+        },
+        options: {
+          scales: {
+            y: {
+              beginAtZero: true,
+              ticks: {
+                color: 'lightgrey', // color of y-axis ticks
+                font: {
+                  family: 'Arial', // font family of y-axis ticks
+                  size: 14, // font size of y-axis ticks
+                  style: 'italic', // font style of y-axis ticks
+                  weight: 'bold', // font weight of y-axis ticks
+                }
+              },
+              grid: {
+                color: 'lightgrey', // color of y-axis grid lines
+                lineWidth: 2 // width of y-axis grid lines
+              }
+            },
+            x: {
+              ticks: {
+                color: 'lightgrey', // color of x-axis ticks
+                font: {
+                  family: 'Arial', // font family of x-axis ticks
+                  size: 14, // font size of x-axis ticks
+                  style: 'italic', // font style of x-axis ticks
+                  weight: 'bold', // font weight of x-axis ticks
+                }
+              },
+              grid: {
+                color: 'lightgrey', // color of x-axis grid lines
+                lineWidth: 0 // width of x-axis grid lines
+              }
+            }
+          },
+          plugins: {
+            legend: {
+              labels: {
+                color: 'lightgrey', // color of legend text
+                font: {
+                  family: 'Arial', // font family of legend text
+                  size: 14, // font size of legend text
+                  style: 'italic', // font style of legend text
+                  weight: 'bold', // font weight of legend text
+                }
+              }
+            }
+          }
+        }
+      });      
 }
