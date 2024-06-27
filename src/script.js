f56f84fa81f2253b4a9181e482da1edae940311b
Kieran-Greeley <kieran.greeley@gmail.com>
2024-06-26 20:39:15 -0400

    added top track, a chart of their popularity, and an average

--- src/script.js
+++ src/script.js
@@ -10,8 +10,10 @@ if (!code) {
     const profile = await fetchProfile(accessToken);
     console.log(profile);
     const artists = await fetchTopArtist(accessToken);
+    const tracks = await fetchTopTracks(accessToken);
     console.log(artists);
-    populateUI(profile, artists);
+    console.log(tracks);
+    populateUI(profile, artists, tracks);
 }
 
 
@@ -88,7 +90,15 @@ async function fetchTopArtist(token){
     return await result.json();
 }
 
-function createCard(name, image){
+async function fetchTopTracks(token){
+  const result = await fetch("https://api.spotify.com/v1/me/top/tracks", {
+      method: "GET", headers: { Authorization: `Bearer ${token}` }
+  });
+
+  return await result.json();
+}
+
+function createCard(name, image, type){
     let card = document.createElement("div");
     card.className = "card"
     let picture = document.createElement("img");
@@ -97,10 +107,11 @@ function createCard(name, image){
     let card_name = document.createElement("h3");
     card_name.innerHTML = name;
     card.appendChild(card_name);
-    document.getElementById("artists_cards").appendChild(card);
+    let type_name = type + "_cards"
+    document.getElementById(type_name).appendChild(card);
 }
 
-function populateUI(profile, artists) {
+function populateUI(profile, artists, tracks) {
     document.getElementById("displayName").innerText = profile.display_name;
     if (profile.images[0]) {
         const profileImage = new Image(200, 200);
@@ -118,12 +129,12 @@ function populateUI(profile, artists) {
     let artist_name = [];
     let artist_pop = [];
     let artist_pic = [];
-    let index = 1;
+    let artist_index = 1;
     let average_pop = 0;
     for(let i of artists.items){
         artist_name.push(i.name);
         artist_pop.push(i.popularity);
-        artist_pic.push(i.images[0].url);
+        //artist_pic.push(i.images[0].url);
         //let artist = document.createElement("li");
         // artist.innerHTML = i.name;
         // document.getElementById("top-artists").appendChild(artist);
@@ -131,18 +142,35 @@ function populateUI(profile, artists) {
         // let picture = document.createElement("img");
         // picture.src = i.images[0].url;S
         // document.getElementById("top-artists").appendChild(picture);
-        let name = index + ". " + i.name;
-        createCard(name, i.images[0].url);
-        index++;
+        //let artist_name = artist_index + ". " + i.name;
+        createCard(artist_index + ". " + i.name, i.images[0].url, "artists");
+        artist_index++;
     }
     average_pop = average_pop/artists.limit; 
-    let avg = document.createElement("p");
-    avg.innerHTML = "your average popularity score is: " + average_pop;
-    document.getElementById("artists").appendChild(avg);
+    let artist_avg = document.createElement("p");
+    artist_avg.innerHTML = "your average popularity score is: " + average_pop;
+    document.getElementById("artists").appendChild(artist_avg);
 
-    const ctx = document.getElementById('artists-graph');
+    let song_name = [];
+    let song_pop = [];
+    let song_index = 1;
+    average_pop = 0;
+    for(let i of tracks.items){
+      average_pop += i.popularity;
+      song_pop.push(i.popularity);
+      song_name.push(i.name);
+      createCard(song_index + ". " + i.name, i.album.images[0].url, "tracks");
+      song_index++;
+    }
+    average_pop = average_pop/tracks.limit;
+    let song_avg = document.createElement("p");
+    song_avg.innerHTML = "your average song popularity score is: " + average_pop;
+    document.getElementById("tracks").appendChild(song_avg);
 
-    new Chart(ctx, {
+    const artist_chart1 = document.getElementById('artists-popularity-bargraph');
+    const tracks_chart1 = document.getElementById('tracks-popularity-bargraph');
+
+    new Chart(artist_chart1, {
         type: 'bar',
         data: {
           labels: artist_name,
@@ -202,5 +230,67 @@ function populateUI(profile, artists) {
             }
           }
         }
-      });      
+      }); 
+      
+      new Chart(tracks_chart1, {
+        type: 'bar',
+        data: {
+          labels: song_name,
+          datasets: [{
+            label: 'Popularity Score',
+            data: song_pop,
+            backgroundColor: 'rgba(20, 205, 86, 1)',
+            //borderColor: 'rgb(20, 205, 86)',
+            borderWidth: 1
+          }]
+        },
+        options: {
+          scales: {
+            y: {
+              beginAtZero: true,
+              ticks: {
+                color: 'lightgrey', // color of y-axis ticks
+                font: {
+                  family: 'Arial', // font family of y-axis ticks
+                  size: 14, // font size of y-axis ticks
+                  style: 'italic', // font style of y-axis ticks
+                  weight: 'bold', // font weight of y-axis ticks
+                }
+              },
+              grid: {
+                color: 'lightgrey', // color of y-axis grid lines
+                lineWidth: 2 // width of y-axis grid lines
+              }
+            },
+            x: {
+              ticks: {
+                color: 'lightgrey', // color of x-axis ticks
+                font: {
+                  family: 'Arial', // font family of x-axis ticks
+                  size: 14, // font size of x-axis ticks
+                  style: 'italic', // font style of x-axis ticks
+                  weight: 'bold', // font weight of x-axis ticks
+                }
+              },
+              grid: {
+                color: 'lightgrey', // color of x-axis grid lines
+                lineWidth: 0 // width of x-axis grid lines
+              }
+            }
+          },
+          plugins: {
+            legend: {
+              labels: {
+                color: 'lightgrey', // color of legend text
+                font: {
+                  family: 'Arial', // font family of legend text
+                  size: 14, // font size of legend text
+                  style: 'italic', // font style of legend text
+                  weight: 'bold', // font weight of legend text
+                }
+              }
+            }
+          }
+        }
+      });
 }
