dac0da36400e9284d4a800556d8e82b60c419812
Kieran-Greeley <kieran.greeley@gmail.com>
2024-06-27 16:26:10 -0400

    added another chart and made the cards look nicer

--- src/script.js
+++ src/script.js
@@ -83,69 +83,79 @@ async function fetchProfile(token) {
 }
 
 async function fetchTopArtist(token){
-    const result = await fetch("https://api.spotify.com/v1/me/top/artists", {
-        method: "GET", headers: { Authorization: `Bearer ${token}` }
-    });
+  const result = await fetch("https://api.spotify.com/v1/me/top/artists", {
+    method: "GET", headers: { Authorization: `Bearer ${token}` }
+  });
 
-    return await result.json();
+  return await result.json();
 }
 
 async function fetchTopTracks(token){
   const result = await fetch("https://api.spotify.com/v1/me/top/tracks", {
-      method: "GET", headers: { Authorization: `Bearer ${token}` }
+    method: "GET", headers: { Authorization: `Bearer ${token}` }
   });
 
   return await result.json();
 }
 
 function createCard(name, image, type){
-    let card = document.createElement("div");
-    card.className = "card"
-    let picture = document.createElement("img");
-    picture.src = image;
-    card.appendChild(picture);
-    let card_name = document.createElement("h3");
-    card_name.innerHTML = name;
-    card.appendChild(card_name);
-    let type_name = type + "_cards"
-    document.getElementById(type_name).appendChild(card);
+  //create padding
+  const outerPadding = document.createElement('div');
+  outerPadding.className = "inline-block px-2";
+
+  //create card
+  let card = document.createElement("div");
+  card.className = "w-64 max-w-xs bg-white border border-gray-200 rounded-lg shadow-lg dark:bg-gray-800 dark:border-gray-700 overflow-hidden hover:shadow-2xl transition-shadow duration-300 ease-in-out";
+
+  //adding pictures to card
+  let picture = document.createElement("img");
+  picture.className = "rounded-t-lg";
+  picture.src = image;
+  card.appendChild(picture);
+
+  //creating space between picture and name
+  const innerPadding = document.createElement('div');
+  innerPadding.className = "p-3";
+
+  //adding name to card
+  let card_name = document.createElement("h5");
+  card_name.innerHTML = name;
+  card_name.className = "mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white";
+  innerPadding.appendChild(card_name);
+
+  card.appendChild(innerPadding);
+  outerPadding.appendChild(card);
+  let type_name = type + "_cards"
+  document.getElementById(type_name).appendChild(outerPadding);
 }
 
 function populateUI(profile, artists, tracks) {
-    document.getElementById("displayName").innerText = profile.display_name;
-    if (profile.images[0]) {
-        const profileImage = new Image(200, 200);
-        profileImage.src = profile.images[0].url;
-        document.getElementById("avatar").appendChild(profileImage);
-        document.getElementById("imgUrl").innerText = profile.images[0].url;
-    }
-    document.getElementById("id").innerText = profile.id;
-    document.getElementById("email").innerText = profile.email;
-    document.getElementById("uri").innerText = profile.uri;
-    document.getElementById("uri").setAttribute("href", profile.external_urls.spotify);
-    document.getElementById("url").innerText = profile.href;
-    document.getElementById("url").setAttribute("href", profile.href);
+  document.getElementById("displayName").innerText = profile.display_name;
+  if (profile.images[0]) {
+    const profileImage = new Image(200, 200);
+    profileImage.src = profile.images[0].url;
+    document.getElementById("avatar").appendChild(profileImage);
+    document.getElementById("imgUrl").innerText = profile.images[0].url;
+  }
+  document.getElementById("id").innerText = profile.id;
+  document.getElementById("email").innerText = profile.email;
+  document.getElementById("uri").innerText = profile.uri;
+  document.getElementById("uri").setAttribute("href", profile.external_urls.spotify);
+  document.getElementById("url").innerText = profile.href;
+  document.getElementById("url").setAttribute("href", profile.href);
 
-    let artist_name = [];
-    let artist_pop = [];
-    let artist_pic = [];
-    let artist_index = 1;
-    let average_pop = 0;
-    for(let i of artists.items){
-        artist_name.push(i.name);
-        artist_pop.push(i.popularity);
-        //artist_pic.push(i.images[0].url);
-        //let artist = document.createElement("li");
-        // artist.innerHTML = i.name;
-        // document.getElementById("top-artists").appendChild(artist);
-        average_pop += i.popularity;
-        // let picture = document.createElement("img");
-        // picture.src = i.images[0].url;S
-        // document.getElementById("top-artists").appendChild(picture);
-        //let artist_name = artist_index + ". " + i.name;
-        createCard(artist_index + ". " + i.name, i.images[0].url, "artists");
-        artist_index++;
-    }
+  let artist_name = [];
+  let artist_pop = [];
+  let artist_pic = [];
+  let artist_index = 1;
+  let average_pop = 0;
+  for(let i of artists.items){
+    artist_name.push(i.name);
+    artist_pop.push(i.popularity);
+    average_pop += i.popularity;
+    createCard("#" + artist_index + ": " + i.name, i.images[0].url, "artists");
+    artist_index++;
+  }
     average_pop = average_pop/artists.limit; 
     let artist_avg = document.createElement("p");
     artist_avg.innerHTML = "your average artist popularity score is: " + average_pop;
@@ -167,7 +177,7 @@ function populateUI(profile, artists, tracks) {
           song_artists_dict[j.name] = 1;
         }
       }
-      createCard(song_index + ". " + i.name, i.album.images[0].url, "tracks");
+      createCard("#" + song_index + ": " + i.name, i.album.images[0].url, "tracks");
       song_index++;
     }
     console.log(song_artists_dict);
@@ -176,11 +186,13 @@ function populateUI(profile, artists, tracks) {
     song_avg.innerHTML = "your average song popularity score is: " + average_pop;
     document.getElementById("tracks_avg_popularity").appendChild(song_avg);
 
-    const artist_chart1 = document.getElementById('artists-popularity-bargraph');
-    const tracks_chart1 = document.getElementById('tracks-popularity-bargraph');
-    const tracks_chart2 = document.getElementById('tracks-artists-piechart');
+    const artist_popularity_chart = document.getElementById('artists-popularity-bargraph');
+    const tracks_popularity_graph = document.getElementById('tracks-popularity-bargraph');
+    const tracks_artists_chart1 = document.getElementById('tracks-artists-barchart');
+    const tracks_artists_chart2 = document.getElementById('tracks-artists-piechart');
 
-    new Chart(artist_chart1, {
+    //charts
+    new Chart(artist_popularity_chart, {
         type: 'bar',
         data: {
           labels: artist_name,
@@ -188,15 +200,16 @@ function populateUI(profile, artists, tracks) {
             label: 'Popularity Score',
             data: artist_pop,
             backgroundColor: 'rgba(20, 205, 86, 1)',
-            //borderColor: 'rgb(20, 205, 86)',
             borderWidth: 1
           }]
         },
         options: {
           scales: {
             y: {
+              suggestedMax: 100,
               beginAtZero: true,
               ticks: {
+                stepSize: 10,
                 color: 'lightgrey', // color of y-axis ticks
                 font: {
                   family: 'Arial', // font family of y-axis ticks
@@ -242,13 +255,77 @@ function populateUI(profile, artists, tracks) {
         }
       }); 
       
-    new Chart(tracks_chart1, {
+    new Chart(tracks_popularity_graph, {
       type: 'bar',
       data: {
         labels: song_name,
         datasets: [{
           label: 'Popularity Score',
           data: song_pop,
+          backgroundColor: 'rgba(20, 205, 86, 1)',
+          //borderColor: 'rgb(20, 205, 86)',
+          borderWidth: 1
+        }]
+      },
+      options: {
+        scales: {
+          y: {
+            suggestedMax: 100,
+            beginAtZero: true,
+            ticks: {
+              stepSize: 10,
+              color: 'lightgrey', // color of y-axis ticks
+              font: {
+                family: 'Arial', // font family of y-axis ticks
+                size: 14, // font size of y-axis ticks
+                style: 'italic', // font style of y-axis ticks
+                weight: 'bold', // font weight of y-axis ticks
+              }
+            },
+            grid: {
+              color: 'lightgrey', // color of y-axis grid lines
+              lineWidth: 2 // width of y-axis grid lines
+            }
+          },
+          x: {
+            ticks: {
+              color: 'lightgrey', // color of x-axis ticks
+              font: {
+                family: 'Arial', // font family of x-axis ticks
+                size: 14, // font size of x-axis ticks
+                style: 'italic', // font style of x-axis ticks
+                weight: 'bold', // font weight of x-axis ticks
+              }
+            },
+            grid: {
+              color: 'lightgrey', // color of x-axis grid lines
+              lineWidth: 0 // width of x-axis grid lines
+            }
+          }
+        },
+        plugins: {
+          legend: {
+            labels: {
+              color: 'lightgrey', // color of legend text
+              font: {
+                family: 'Arial', // font family of legend text
+                size: 14, // font size of legend text
+                style: 'italic', // font style of legend text
+                weight: 'bold', // font weight of legend text
+              }
+            }
+          }
+        }
+      }
+    });
+
+    new Chart(tracks_artists_chart1, {
+      type: 'bar',
+      data: {
+        labels: Object.keys(song_artists_dict),
+        datasets: [{
+          label: 'Number of songs per artist',
+          data: Object.values(song_artists_dict),
           backgroundColor: 'rgba(20, 205, 86, 1)',
           //borderColor: 'rgb(20, 205, 86)',
           borderWidth: 1
@@ -304,7 +381,7 @@ function populateUI(profile, artists, tracks) {
       }
     });
 
-    new Chart(tracks_chart2, {
+    new Chart(tracks_artists_chart2, {
       type: 'pie',
       data: {
           labels: Object.keys(song_artists_dict),
